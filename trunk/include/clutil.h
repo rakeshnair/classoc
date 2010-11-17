ec928e568ed979df760bcee118444d82f75b5230
girish574 <girish574@0a58cef7-18e4-e94d-92c4-9843f5c9dda5>
2010-11-17 18:12:25 +0000

    ocl framework

---
+++ trunk/include/clutil.h
@@ -0,0 +1,70 @@
+/*
+   File: clutil.h
+ */
+
+#ifndef __CLUTIL__
+#define __CLUTIL__
+
+#include <CL/cl.h>
+#include<iostream>
+#include <sys/time.h>
+#include<sys/resource.h>
+#include <sys/types.h>
+#include<string>
+using namespace std;
+
+
+unsigned initExecution();
+void waitForEvent();
+void print(const unsigned, const size_t, const size_t);
+
+void checkError(const cl_int ciErrNum, const char* const operation);
+const char* oclErrorString(cl_int error);
+
+void verifyResult(const unsigned size);
+
+cl_kernel getKernel(const unsigned index);
+
+void init_cl_context();
+
+cl_uint getDeviceCount();
+
+void createCommandQueue();
+void
+setKernelArg(const string& kernel, const size_t argSize, const void* argVal);
+
+
+void buildError();
+char*
+readFile(const char* headerFileName, const char* srcFileName,  
+                   size_t& length);
+
+void compileProgram(const char* const header_file,
+                    const char* const kernel_file);
+void* allocateHostMemory(const unsigned size);
+
+
+void createKernel(const string&);
+
+
+void runKernel(const string&, const size_t localWorkSize,
+               const size_t globalWorkSize);
+void copyToDevice(const cl_mem mem,
+                  void* const hostPtr, const unsigned size);
+
+
+void copyFromDevice(const cl_mem dMem,
+                    void* hostPtr, const unsigned size);
+
+double executionTime();
+cl_mem allocateDeviceMemory(void* buffer, const unsigned size, cl_mem_flags);
+                          
+
+void printGpuTime();
+
+void cleanup();
+
+
+#endif
+
+
