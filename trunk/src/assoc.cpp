f5b7aaf7da2b817d512b56ae8a3cf6ff71cfe916
girish574 <girish574@0a58cef7-18e4-e94d-92c4-9843f5c9dda5>
2010-12-03 16:34:57 +0000

    bitmap

--- trunk/src/assoc.cpp
+++ trunk/src/assoc.cpp
@@ -1,6 +1,7 @@
 #include<assoc.h>
 #include<clutil.h>
 #include<assert.h>
+#include<bitset>
 
 
 void
@@ -36,7 +37,7 @@ gpuAssoc(const char* file)
     
     runKernel("itemCount1", szLocalWorkSize, szGlobalWorkSize);
 #endif
-#if 1
+#if 0
     createKernel("itemCount2");
     setKernelArg("itemCount2", 0, sizeof(cl_mem), &dMarketBasket);
     setKernelArg("itemCount2", 1, sizeof(cl_mem), &dCount);
@@ -49,6 +50,21 @@ gpuAssoc(const char* file)
 
     runKernel("itemCount2", szLocalWorkSize, szGlobalWorkSize); 
 #endif
+#if 1 
+    unsigned* buff = (unsigned*)allocateHostMemory(sizeof(unsigned) * 256);
+    for (unsigned i = 0; i < 256; ++i) {
+        buff[i] = 255;
+    }
+    cl_mem dBuff = allocateDeviceMemory(buff, sizeof(unsigned) * 256,
+                                               CL_MEM_READ_ONLY);
+    createKernel("testbit");
+    setKernelArg("testbit", 0, sizeof(cl_mem), &dBuff);
+
+    size_t szLocalWorkSize = NO_THREADS_BLOCK;
+    size_t szGlobalWorkSize = NO_THREADS_BLOCK;
+
+    runKernel("testbit", szLocalWorkSize, szGlobalWorkSize);
+#endif
     waitForEvent();
     copyFromDevice(dCount, count, nItems * sizeof(int));
 
@@ -57,6 +73,45 @@ gpuAssoc(const char* file)
     verifyCount(count);
     cleanup();
 }
+
+void
+gpuAssocBitmap(const char* file)
+{
+    fileReadBitmap(file);
+    initExecution();
+    int* count = (int*)allocateHostMemory(nItems * sizeof(int));
+    for (unsigned i = 0; i < nItems; ++i) {
+        count[i] = 0;
+    }
+    const int marketBasketSize = sizeof(unsigned) * nItems * nIntegers;
+    cl_mem dMarketBasketBitmap = allocateDeviceMemory(marketBasketBitmap, 
+                                                 marketBasketSize,
+                                                CL_MEM_READ_ONLY);
+    cl_mem dCount = allocateDeviceMemory(count, nItems * sizeof(int),
+                                               CL_MEM_WRITE_ONLY);
+
+    cl_mem dBitLookup = allocateDeviceMemory(bitLookup, sizeof(uchar) * 65536,
+                                             CL_MEM_READ_ONLY);
+    compileProgram(0, "kernel.cl");
+    createKernel("itemCount3");
+    setKernelArg("itemCount3", 0, sizeof(cl_mem), &dMarketBasketBitmap);
+    setKernelArg("itemCount3", 1, sizeof(cl_mem), &dCount);
+    setKernelArg("itemCount3", 2, sizeof(cl_mem), &dBitLookup);
+    setKernelArg("itemCount3", 3, sizeof(int), 0);
+    setKernelArg("itemCount3", 4, sizeof(unsigned), (void*)&nIntegers);
+    setKernelArg("itemCount3", 5, sizeof(unsigned), (void*)&nItems);
+
+    size_t szLocalWorkSize = NO_THREADS_BLOCK;
+    size_t szGlobalWorkSize = nItems * NO_THREADS_BLOCK;
+
+    runKernel("itemCount3", szLocalWorkSize, szGlobalWorkSize);
+    waitForEvent();
+    copyFromDevice(dCount, count, nItems * sizeof(int));
+    printGpuTime();
+    verifyBitmapCount(count);
+    cleanup();
+}
+
 
 void 
 verifyCount(int * count)
@@ -75,6 +130,24 @@ verifyCount(int * count)
            return;
         } 
 	}
+    cout << "Verified" << endl << endl;
+}
+
+void
+verifyBitmapCount(int* count)
+{
+   for (unsigned k = 0; k < nItems; ++k) {
+	   unsigned countI = 0;
+	   for (unsigned i = 0; i < nIntegers; ++i ) {
+		   bitset<32> bTrans=  marketBasketBitmap[(k * nIntegers) + i];
+		   countI += bTrans.count();
+	   }
+       if (countI != count[k]) {
+           cout << "**********ERROR in item " << k <<  endl << endl;;
+           cout << "Actual: "  << countI << "Result: " << count[k] << endl;
+           return;
+        }
+    }
     cout << "Verified" << endl << endl;
 }
 
@@ -121,3 +194,4 @@ test()
     printGpuTime();
     cleanup();
 }
+
--- trunk/src/fileread.cpp
+++ trunk/src/fileread.cpp
@@ -6,11 +6,17 @@
 #include<assoc.h>
 #include<assert.h>
 #include<clutil.h>
+#include<bitset>
+#include<string.h>
 using namespace std;
 
-bool* marketBasket;
+bool* marketBasket = 0;;
 unsigned nTransactions = 0;
 unsigned nItems = 0;
+unsigned nIntegers = 0;
+unsigned* marketBasketBitmap = 0;
+typedef unsigned char uchar;
+uchar* bitLookup = 0;
 
 
 void
@@ -58,6 +64,80 @@ fileRead(const char* file)
        }
        ++tTrans;  
     }
-
+   
     fp.close();
 }
+
+
+void
+fileReadBitmap(const char* file)
+{
+    ifstream fp;
+    fp.open(file, ios::in);
+    if (!fp.is_open()) {
+        cout <<  "Not Able to Open file " << file << endl;
+        exit(0);
+    }
+    while(!fp.eof()) {
+        string line;
+        getline(fp, line);
+        if (line[0] == '\0') break;
+        stringstream s(line);
+        unsigned item;
+        while (!s.eof()) {
+            s >> item;
+            if (item > nItems) nItems = item;
+            //if (s.eof()) break;
+        }
+        ++nTransactions;
+    }
+    ++nItems; // 0 is an item
+    cout << "Market Basket File: "  << file << endl
+         << "Transcations: " << nTransactions << endl
+         << "Items: " << nItems << endl;
+
+    fp.close();
+    fp.open(MARKET_BASKET_FILE, ios::in);
+    if (nTransactions % 32) {
+        nIntegers = (nTransactions / 32) + 1;
+    } else {
+        nIntegers = nTransactions / 32;
+    }
+    
+    marketBasketBitmap = (unsigned*)allocateHostMemory(sizeof(int) * nIntegers *
+                                                         nItems);
+    memset(marketBasketBitmap, 0, sizeof(int) * nIntegers * nItems);
+    unsigned tTrans = 0;
+    while(!fp.eof()) {
+       string line;
+       getline(fp, line);
+       if (line[0] == '\0') break;
+       stringstream s(line);
+       unsigned item;
+       while (!s.eof()) {
+           s >> item;
+           bitset<32> bTrans(marketBasketBitmap[(item * nIntegers) + tTrans / 32]); 
+           bTrans.set(tTrans % 32, 1);
+           marketBasketBitmap[(item * nIntegers) + tTrans / 32] = bTrans.to_ulong();
+       }
+       ++tTrans;
+    }
+#if 0
+     unsigned countI = 0;
+		for (unsigned i = 0; i < nIntegers; ++i ) {
+			bitset<32> bTrans=  marketBasketBitmap[(74 * nIntegers) + i]; 
+   		    countI += bTrans.count();
+		}
+        cout << "Bitmap" << countI << endl;
+
+#endif
+    bitLookup = (uchar*)allocateHostMemory(sizeof(uchar) * 65536);
+    for (unsigned i = 0; i < 65536; ++i) {
+        bitset<16> bits(i);
+        bitLookup[i] = (uchar)bits.count();
+    }
+    fp.close();
+}
+
+    
+
