0b275055cb0acfd35716133c9a1f5c8bc15a1f2a
rejith.g <rejith.g@0a58cef7-18e4-e94d-92c4-9843f5c9dda5>
2010-12-06 02:24:33 +0000

    Adding primes

---
+++ src_rejisu/src1/primes.h
@@ -0,0 +1,10 @@
+#ifndef PRIMES_H
+#define PRIMES_H
+#define SUPPORT_COUNT 300
+extern int gNoOfPrimes;
+extern  unsigned long *gTransactionSet;
+extern unsigned gNTranscations;
+extern unsigned gNItems;
+extern int  primes[];
+void mPrintFreqItems(int * ,int );
+#endif
